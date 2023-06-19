fd733c49c797fb4ab54ff776828b1ba9de32a6c5
gaokang <gaokang08@qq.com>
2023-06-19 16:17:29 +0800

    add dataSource to dag

--- src/main/java/com/chainmaker/jobservice/api/service/impl/JobParserServiceImpl.java
+++ src/main/java/com/chainmaker/jobservice/api/service/impl/JobParserServiceImpl.java
@@ -123,7 +123,7 @@ public class JobParserServiceImpl implements JobParserService {
         JobGraphVo jobGraphVo = new JobGraphVo();
         JobGraph jobGraph = new JobGraph();
         if (jobInfo.getTasks() != null) {
-            Dag dag = taskToDag(jobInfo.getTasks());
+            Dag dag = taskToDag(jobInfo.getTasks(), jobTemplate.getJob().getStatus());
             jobGraphVo.setDag(dag);
             jobGraph.setDag(dag);
         }
@@ -231,7 +231,7 @@ public class JobParserServiceImpl implements JobParserService {
             jobGraphVo.setTopology(topology);
         }
         if (jobInfo.getTasks() != null) {
-            Dag dag = taskToDag(jobInfo.getTasks());
+            Dag dag = taskToDag(jobInfo.getTasks(), jobInfoPo.getJob().getStatus());
             jobGraphVo.setDag(dag);
         }
         jobGraphVo.setJobInfo(jobInfoVo);
@@ -248,7 +248,7 @@ public class JobParserServiceImpl implements JobParserService {
 
         JobGraphVo jobGraphVo = new JobGraphVo();
         if (jobInfo.getTasks() != null) {
-            Dag dag = taskToDag(jobInfo.getTasks());
+            Dag dag = taskToDag(jobInfo.getTasks(), jobInfoPo.getJob().getStatus());
             jobGraphVo.setDag(dag);
         }
         if (jobInfo.getServices() != null) {
@@ -344,11 +344,13 @@ public class JobParserServiceImpl implements JobParserService {
 
 
     @Override
-    public Dag taskToDag(List<Task> tasks) {
+    public Dag taskToDag(List<Task> tasks, String dataStatus) {
         Dag dag = new Dag();
         List<DagNode> nodes = new ArrayList<>();
         List<DagEdge> edges = new ArrayList<>();
-        int id = 0;
+        int capacity = 100;
+        int dataCount = 0;
+        int id = capacity;
         for (Task task : tasks) {
             DagNode dagNode = new DagNode();
             dagNode.setId(id);
@@ -364,10 +366,27 @@ public class JobParserServiceImpl implements JobParserService {
             for (TaskInputData taskInputData: task.getInput().getData()) {
                 if (!Objects.equals(taskInputData.getTaskSrc(), "")) {
                     DagEdge dagEdge = new DagEdge();
-                    dagEdge.setFrom(Integer.parseInt(taskInputData.getTaskSrc()));
-                    dagEdge.setTo(Integer.parseInt(taskName));
+                    dagEdge.setFrom(Integer.parseInt(taskInputData.getTaskSrc()) + capacity);
+                    dagEdge.setTo(Integer.parseInt(taskName) + capacity);
                     dagEdge.setLabel(taskInputData.getDataName());
                     edges.add(dagEdge);
+                } else {
+                    DagNode dataNode = new DagNode();
+                    dataNode.setId(dataCount);
+                    dataCount += 1;
+                    String dataLabel = taskInputData.getDataName() + "(" + dataStatus + ")";
+                    dataNode.setLabel(dataLabel);
+                    dataNode.setShape("image");
+                    dataNode.setImage("dataSource");
+                    dataNode.setFindLabel(task.getTaskName());
+                    nodes.add(dataNode);
+
+                    DagEdge dataEdge = new DagEdge();
+                    dataEdge.setFrom(dataCount);
+                    dataEdge.setTo(Integer.parseInt(taskName) + capacity);
+                    dataEdge.setLabel(taskInputData.getDataName());
+                    edges.add(dataEdge);
+
                 }
             }
         }
