c53cdb3f36e33686ab998c6251f3271eea533c4c
JdaDong <601984381@qq.com>
2024-03-01 11:46:50 +0800

    fix parsesql's stream process

--- simple_run.sh
+++ simple_run.sh
@@ -1,1 +1,1 @@
-git pull && mvn clean package &&docker build -f ./Dockerfile-simpe -t docker.oa.com:5000/mpc/job-service:v2.0.0 .
+git pull && mvn clean package &&docker build -f ./Dockerfile-simple -t docker.oa.com:5000/mpc/job-service:v2.0.0 .
