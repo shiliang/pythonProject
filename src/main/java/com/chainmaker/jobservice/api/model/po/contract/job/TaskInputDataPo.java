44f2cbf7ead20d397025bb3fe94b7ff0bdd20aed
JdaDong <601984381@qq.com>
2024-03-05 13:27:22 +0800

    fix assetName

--- src/main/java/com/chainmaker/jobservice/api/model/po/contract/job/TaskInputDataPo.java
+++ src/main/java/com/chainmaker/jobservice/api/model/po/contract/job/TaskInputDataPo.java
@@ -1,12 +1,14 @@
 package com.chainmaker.jobservice.api.model.po.contract.job;
 
+import lombok.Data;
+
 /**
  * @author gaokang
  * @date 2022-09-19 19:27
  * @description:
  * @version:
  */
-
+@Data
 public class TaskInputDataPo {
     private String dataName;
     private String taskSrc;
@@ -14,64 +16,11 @@ public class TaskInputDataPo {
     private String domainID;
     private String role;
     private String params;
-
-    public String getDataName() {
-        return dataName;
-    }
-
-    public void setDataName(String dataName) {
-        this.dataName = dataName;
-    }
-
-    public String getTaskSrc() {
-        return taskSrc;
-    }
-
-    public void setTaskSrc(String taskSrc) {
-        this.taskSrc = taskSrc;
-    }
-
-    public String getDataID() {
-        return dataID;
-    }
-
-    public void setDataID(String dataID) {
-        this.dataID = dataID;
-    }
-
-    public String getDomainID() {
-        return domainID;
-    }
-
-    public void setDomainID(String domainID) {
-        this.domainID = domainID;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getParams() {
-        return params;
-    }
-
-    public void setParams(String params) {
-        this.params = params;
-    }
-
-    @Override
-    public String toString() {
-        return "TaskInputDataPo{" +
-                "dataName='" + dataName + '\'' +
-                ", taskSrc='" + taskSrc + '\'' +
-                ", dataID='" + dataID + '\'' +
-                ", domainID='" + domainID + '\'' +
-                ", role='" + role + '\'' +
-                ", params='" + params + '\'' +
-                '}';
-    }
+    private String assetName;
+    private String databaseName;
+    private String tableName;
+    private String columnName;
+    private String type;
+    private String length;
+    private String comments;
 }
