06e0d27f8c0d3c84f68f5cff10cd1bcc5327e60d
pc <jrome258@outlook.com>
2024-07-09 18:35:40 +0800

    add prometheus monitor

--- src/main/java/com/chainmaker/jobservice/api/response/ResponseResultConfig.java
+++ src/main/java/com/chainmaker/jobservice/api/response/ResponseResultConfig.java
@@ -1,35 +1,35 @@
-package com.chainmaker.jobservice.api.response;
-
-import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.alibaba.fastjson.support.config.FastJsonConfig;
-import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.AbstractGenericHttpMessageConverter;
-import org.springframework.http.converter.AbstractHttpMessageConverter;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-@Configuration
-public class ResponseResultConfig implements WebMvcConfigurer {
-
-    @Override
-    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.clear();
-        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
-        FastJsonConfig fastJsonConfig = new FastJsonConfig();
-        fastJsonConfig.setSerializerFeatures(SerializerFeature.DisableCircularReferenceDetect);
-        converter.setFastJsonConfig(fastJsonConfig);
-        converter.setSupportedMediaTypes(List.of(MediaType.APPLICATION_JSON));
-        converters.add(0, converter); // 添加到首位以优先使用
-    }
-
-}
+//package com.chainmaker.jobservice.api.response;
+//
+//import com.alibaba.fastjson.serializer.SerializerFeature;
+//import com.alibaba.fastjson.support.config.FastJsonConfig;
+//import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.MediaType;
+//import org.springframework.http.converter.AbstractGenericHttpMessageConverter;
+//import org.springframework.http.converter.AbstractHttpMessageConverter;
+//import org.springframework.http.converter.HttpMessageConverter;
+//import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+//import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+//
+//@Configuration
+//public class ResponseResultConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+//        converters.clear();
+//        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+//        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+//        fastJsonConfig.setSerializerFeatures(SerializerFeature.DisableCircularReferenceDetect);
+//        converter.setFastJsonConfig(fastJsonConfig);
+//        converter.setSupportedMediaTypes(List.of(MediaType.APPLICATION_JSON));
+//        converters.add(0, converter); // 添加到首位以优先使用
+//    }
+//
+//}
